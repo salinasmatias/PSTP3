84097960a38830f54bd1c8ececdac6451055b0af
Matías Salinas <salinasmatias1990@gmail.com>
2021-12-10 13:36:26 -0300

    saving changes

--- public/js/components/venta.js
+++ public/js/components/venta.js
@@ -3,13 +3,13 @@ export const venta = (titulo, poster, sinopsis) => `
             <div class="col-md-12">
                 <div class="row">
                     <div class="col-md-12 text-center">
-                        <h3>
+                        <h3 id="pelicula-titulo">
                             ${titulo}
                         </h3>
                         <div class="row">
                           <img alt="Bootstrap Image Preview" src="${poster}" id="imagen-pelicula" class="rounded mx-auto"/>
                         </div>
-                        <div class="row">
+                        <div class="row" id="pelicula-sinopsis">
                           <p>${sinopsis}</p>
                         </div>
                     </div>
